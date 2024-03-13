caa0e2798e08b8c8acbf2c0446e260176b6fa53f
jgoetz <jgoetz@tmcc.edu>
2024-03-12 20:42:39 -0700

    set the number of projectiles to 40

--- Projectile/Program.cs
+++ Projectile/Program.cs
@@ -46,7 +46,7 @@
 
             Object target = new Object();
            
-            for(int i = 0; i < 20; i++) 
+            for(int i = 0; i < 40; i++) 
             {
                 if (rng.Next() % 2 == 0)
                 {
